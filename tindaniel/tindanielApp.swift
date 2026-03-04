a9af9f2a8943ca3dbeb6e6e7646a9513b49cbf29
daniellberg <danielbergons@gmail.com>
2026-03-03 21:04:54 -0800

    Initial Commit

---
+++ tindaniel/tindanielApp.swift
@@ -0,0 +1,17 @@
+//
+//  tindanielApp.swift
+//  tindaniel
+//
+//  Created by Daniel Berg on 03/03/26.
+//
+
+import SwiftUI
+
+@main
+struct tindanielApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ tindanielTests/tindanielTests.swift
@@ -0,0 +1,17 @@
+//
+//  tindanielTests.swift
+//  tindanielTests
+//
+//  Created by Daniel Berg on 03/03/26.
+//
+
+import Testing
+@testable import tindaniel
+
+struct tindanielTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
