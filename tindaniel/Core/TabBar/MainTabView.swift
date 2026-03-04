78a21e076298967115c31a4b8900e1b11ff314ca
daniellberg <danielbergons@gmail.com>
2026-03-03 21:52:08 -0800

    new: set folder structure and first TabBar

---
+++ tindaniel/Core/TabBar/MainTabView.swift
@@ -0,0 +1,43 @@
+//
+//  MainTabBar.swift
+//  tindaniel
+//
+//  Created by Daniel Berg on 03/03/26.
+//
+
+import SwiftUI
+
+struct MainTabView: View {
+    var body: some View {
+        TabView{
+            Text("Swiping View")
+                .tabItem{
+                    Image(systemName: "flame")
+                }
+                .tag(0)
+            
+            Text("Search View")
+                .tabItem{
+                    Image(systemName: "magnifyingglass")
+                }
+                .tag(1)
+
+            Text("Inbox View")
+                .tabItem{
+                    Image(systemName: "bubble")
+                }
+                .tag(2)
+            
+            Text("Profile View")
+                .tabItem{
+                    Image(systemName: "person")
+                }
+                .tag(3)
+
+        }
+    }
+}
+
+#Preview {
+    MainTabView()
+}
