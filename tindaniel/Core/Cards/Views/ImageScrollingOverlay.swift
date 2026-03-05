e4dfff536f6534c41a1c78298cb4354893702d1e
daniellberg <danielbergons@gmail.com>
2026-03-04 21:38:18 -0800

    add: multiple images per user -> tap to go to next/previous pic

---
+++ tindaniel/Core/Cards/Views/ImageScrollingOverlay.swift
@@ -0,0 +1,18 @@
+//
+//  ImageScrollingOverlay.swift
+//  tindaniel
+//
+//  Created by Daniel Berg on 04/03/26.
+//
+
+import SwiftUI
+
+struct ImageScrollingOverlay: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ImageScrollingOverlay()
+}
